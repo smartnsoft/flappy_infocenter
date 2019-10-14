fc257e306d4379bfd8446c2de819ccca7bf18b34
Ecalle <thomasecalle@hotmail.fr>
2019-10-14 15:59:36 +0200

    Update android package to stop crashing

--- android/src/main/kotlin/com/equinoa/flappy_infocenter/FlappyInfoCenterPlugin.kt
+++ android/src/main/kotlin/com/equinoa/flappy_infocenter/FlappyInfoCenterPlugin.kt
@@ -1,4 +1,4 @@
-package com.equinoa.flappyiosplayernotif.iosnotif
+package com.equinoa.flappy_infocenter
 
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -6,12 +6,12 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class IosnotifPlugin: MethodCallHandler {
+class FlappyInfoCenterPlugin: MethodCallHandler {
   companion object {
     @JvmStatic
     fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "iosnotif")
-      channel.setMethodCallHandler(IosnotifPlugin())
+      val channel = MethodChannel(registrar.messenger(), "flappyInfoCenter")
+      channel.setMethodCallHandler(FlappyInfoCenterPlugin())
     }
   }
 
